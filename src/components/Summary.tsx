3670f5a41a2ab0f8c117e2dbeee3f4758580e076
winwanwon <winwanwon123@gmail.com>
2022-02-19 22:24:14 +0700

    Responsive grid

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -16,6 +16,8 @@ interface OwnProps {
 const Summary: React.FC<OwnProps> = (props: OwnProps) => {
     const { appState, uuid, users } = props;
 
+    const userCount = Object.keys(users).length;
+
     const renderAttendees = Object.keys(users).map((key) => {
         const selected = users[key].selectedOption > -1;
         const isInit = appState === AppState.Init;
@@ -23,60 +25,59 @@ const Summary: React.FC<OwnProps> = (props: OwnProps) => {
         const confirmedValue = selected || isRevealed;
 
         return (
-            <Stack key={key} alignItems="center" marginX={2} marginY={2}>
+            <Box
+                width={150}
+                height={200}
+                className="card"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                alignSelf="center"
+                justifySelf="center"
+                gridColumn="span 1"
+            >
                 <Box
-                    width={160}
-                    height={220}
-                    className="card"
+                    width={84}
+                    height={84}
+                    border={4}
+                    borderColor={confirmedValue ? 'success.light' : 'primary.main'}
+                    borderRadius={42}
                     display="flex"
-                    flexDirection="column"
                     alignItems="center"
                     justifyContent="center"
                 >
-                    <Box
-                        width={84}
-                        height={84}
-                        border={4}
-                        borderColor={confirmedValue ? 'success.light' : 'primary.main'}
-                        borderRadius={42}
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
+                    {isInit && (selected ? <DoneIcon color="success" sx={{ fontSize: 60 }} /> : <QuestionMarkIcon color="primary" sx={{ fontSize: 60 }} />)}
+                    <Typography
+                        color={confirmedValue ? 'success.main' : 'primary.main'}
+                        fontWeight={500}
+                        fontSize={48}
+                        textAlign="center"
                     >
-                        {isInit && (selected ? <DoneIcon color="success" sx={{ fontSize: 60 }} /> : <QuestionMarkIcon color="primary" sx={{ fontSize: 60 }} />)}
-                        <Typography
-                            color={confirmedValue ? 'success.main' : 'primary.main'}
-                            fontWeight={500}
-                            fontSize={48}
-                            textAlign="center"
-                        >
-                            {isRevealed && (selected ? users[key].selectedOption : "-")}
-                        </Typography>
-                    </Box>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        marginTop={2}
-                    >
-                        {key === uuid && <PersonIcon color="primary" />}
-                        <Typography variant="h6" textAlign="center" color={key === uuid ? 'primary' : 'text'}>
-                            {users[key].name}
-                        </Typography>
-                    </Box>
+                        {isRevealed && (selected ? users[key].selectedOption : "-")}
+                    </Typography>
                 </Box>
-            </Stack>
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    marginTop={2}
+                >
+                    {key === uuid && <PersonIcon color="primary" />}
+                    <Typography variant="h6" textAlign="center" color={key === uuid ? 'primary' : 'text'}>
+                        {users[key].name}
+                    </Typography>
+                </Box>
+            </Box>
         );
     });
 
     return (
         <>
             <Stack direction="column" spacing={4}>
-                <Stack
-                    direction="row"
-                    justifyContent="center">
+                <Box display="grid" gridTemplateColumns={`repeat(${userCount <= 6 ? userCount : Math.ceil(userCount / 2)}, ${userCount <= 6 ? 1 : 2}fr)`} gap={2} >
                     {renderAttendees}
-                </Stack>
+                </Box>
             </Stack>
         </>
     );
