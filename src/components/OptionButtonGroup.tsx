6e5f0b3a6e39d0ff87615ee0e434925860b00b60
winwanwon <winwanwon123@gmail.com>
2022-03-15 23:20:21 +0700

    Select option using keyboard

--- src/components/OptionButtonGroup.tsx
+++ src/components/OptionButtonGroup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { Button, ButtonGroup } from '@mui/material';
 import { AppState } from '../enum';
@@ -12,11 +13,33 @@ const OptionButtonGroup: React.FC<OwnProps> = (props: OwnProps) => {
     const { selectedOption, setSelectedOption, appState, onOptionSelect } = props;
     const options = [0, 1, 2, 3, 5, 8, 13];
 
+    React.useEffect(() => {
+        document.body.addEventListener('keydown', (event) => {
+            if (event.repeat) return;
+            const keyMap: { [key: string]: number } = {
+                '1': 0,
+                '2': 1,
+                '3': 2,
+                '4': 3,
+                '5': 4,
+                '6': 5,
+                '7': 6,
+            };
+            if (event.key in keyMap) {
+                selectOption(options[keyMap[event.key]]);
+            }
+        });
+    }, []);
+
+    const selectOption = (option: number) => {
+        const opt = option === selectedOption ? -1 : option;
+        setSelectedOption(opt);
+        onOptionSelect(opt);
+    };
+
     const renderOptions = options.map((option) => {
         const onClick = () => {
-            const opt = option === selectedOption ? -1 : option;
-            setSelectedOption(opt);
-            onOptionSelect(opt);
+            selectOption(option);
         };
         return (
             <Button
