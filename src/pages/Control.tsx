3c6956b881fe080c139c6e74d41f9d075935636a
winwanwon <winwanwon123@gmail.com>
2023-03-11 18:42:55 +0700

    Introduce Pop-out option window

---
+++ src/pages/Control.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { FirebaseApp } from "firebase/app";
+import { getDatabase, onValue, ref, update } from "firebase/database";
+import { Box, Stack, Typography } from "@mui/material";
+import InfoIcon from '@mui/icons-material/Info';
+
+import { AppState } from "../enum";
+import { isValidRoomName } from "../utils";
+
+import OptionButtonGroup from "../components/OptionButtonGroup";
+
+interface Props {
+    firebaseApp: FirebaseApp;
+}
+
+const Control: React.FC<Props> = (props: Props) => {
+    const app = props.firebaseApp;
+    const database = getDatabase(app);
+    const uuid = window.localStorage.getItem("uuid")
+    const name = window.localStorage.getItem("name")
+
+    const params = useParams();
+    const roomName = params.roomName?.toLowerCase() || "";
+    const [appState, setAppState] = useState<AppState>(AppState.Init);
+    const [selectedOption, setSelectedOption] = useState<number>(-1);
+    const stateDbPath = roomName + '/state/';
+    const thisUserDbPath = roomName + '/users/' + uuid;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isValidRoomName(roomName) || !uuid) {
+            navigate('/');
+        }
+    }, [roomName, navigate, uuid]);
+
+
+    useEffect(() => {
+        onValue(ref(database, thisUserDbPath), (snapshot) => {
+            const dbSnap = snapshot.val();
+            dbSnap && setSelectedOption(dbSnap.selectedOption);
+        });
+
+        onValue(ref(database, stateDbPath), (snapshot) => {
+            const dbSnap = snapshot.val();
+            dbSnap && setAppState(dbSnap.currentState);
+        });
+    }, [database, stateDbPath, thisUserDbPath]);
+
+    const onOptionSelect = (option: number) => {
+        update(ref(database, thisUserDbPath), {
+            selectedOption: option,
+        });
+    };
+
+    const optionButtons = (
+        <OptionButtonGroup
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            appState={appState}
+            onOptionSelect={onOptionSelect}
+            visibility={true}
+        />
+    );
+
+    const renderWaitMessage = () => {
+        return (
+            <Typography>
+                Options will be available once the points get reset.
+            </Typography>
+        )
+    }
+
+    return (
+        <>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                width="100%"
+                height="100%"
+            >
+                <Stack
+                    spacing={2}
+                >
+                    <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <InfoIcon sx={{ fontSize: 16}} />
+                        <Typography variant="caption" ml={1}>Selecting an option for</Typography>
+                        <Typography variant="caption" fontWeight={600}>&nbsp; {name}</Typography>
+                    </Box>
+                    {appState === AppState.Init && optionButtons}
+                    {appState === AppState.Revealed && renderWaitMessage()}
+                </Stack>
+            </Box>
+        </>
+    );
+}
+
+export default Control;
