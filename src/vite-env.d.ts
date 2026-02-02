73af652a84378905fa26abd79a4f4acce016d7ea
winwanwon <winwanwon123@gmail.com>
2026-02-02 09:59:20 +0700

    fix: revert update in env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string
-  readonly VITE_FIREBASE_DATABASE_URL: string
-  readonly VITE_FIREBASE_PROJECT_ID: string
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string
-  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
-  readonly VITE_FIREBASE_APP_ID: string
-  readonly VITE_FIREBASE_MEASUREMENT_ID: string
+  readonly REACT_APP_FIREBASE_API_KEY: string
+  readonly REACT_APP_FIREBASE_AUTH_DOMAIN: string
+  readonly REACT_APP_FIREBASE_DATABASE_URL: string
+  readonly REACT_APP_FIREBASE_PROJECT_ID: string
+  readonly REACT_APP_FIREBASE_STORAGE_BUCKET: string
+  readonly REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly REACT_APP_FIREBASE_APP_ID: string
+  readonly REACT_APP_FIREBASE_MEASUREMENT_ID: string
 }
 
 interface ImportMeta {
